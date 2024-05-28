6a493b956ce4fd97517b37aa55546bf2716a397d
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-28 15:34:00 +0200

    added an alert when img is clicked

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ createApp({
     data() {
         return {
             message: 'Hello world!',
-            image: 'https://d.newsweek.com/en/full/1601678/bilbo-baggins.jpg'
-
+            image: 'https://i.pinimg.com/originals/47/04/5c/47045cc00173365b8f2f5c09fb203e59.jpg',
+            
+        }
+    },methods:{
+        showAlert(){
+            alert("The power of the sun in the palm of my hand!")
         }
     }
 }).mount('#app')
