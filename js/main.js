2b4137916c0bff0084f2efc9e35da09f5de785a2
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-28 13:08:19 +0200

    an img now is shown thanks to VueJS utilities

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ createApp({
     data() {
         return {
             message: 'Hello world!',
-            
+            image: 'https://d.newsweek.com/en/full/1601678/bilbo-baggins.jpg'
 
         }
     }
